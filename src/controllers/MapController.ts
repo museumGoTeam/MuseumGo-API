4547a95964a03adbe51844980c17f37efd896ca4
museumGoTeam <museumgo.team@gmail.com>
2020-11-07 18:18:06 +0100

    get point of interests and send it to to the client with the map

--- src/controllers/MapController.ts
+++ src/controllers/MapController.ts
@@ -1,20 +1,24 @@
 import express, { Router } from 'express'
 import { FZ_FILENAME } from '../constants'
 import PoiService from '../services/PoiService'
+import { DocumentResponse, IPOI, IRoom } from '../services/type'
 import FileUtil from '../utils/FileUtil'
 
 const router = Router()
 const _poiContext = new PoiService()
 
 
+
+
 router.get("/", async (req, res) => {
     const cells: number[][]  = await FileUtil.fileToCells(FZ_FILENAME)
-    res.status(200).json(cells)
+    const pois: DocumentResponse= await  _poiContext.getAll()
+    res.status(200).json({map: cells, pois: pois.data, rooms: []})
 })
 
 router.post("/", (req, res) => {
     req.url = "/"
-    const {cells, pois, rooms}: {cells: number[][], pois: string[], rooms: string[]} = req.body
+    const {cells, pois, rooms}: {cells: number[][], pois: IPOI[], rooms: IRoom[]} = req.body
     pois.forEach(async poi => {
         await _poiContext.insertOne(poi)
     })
